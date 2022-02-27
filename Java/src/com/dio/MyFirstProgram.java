d7951103bd83a11a63f580c59ff1592e8f1aacec
guiquintero <guiquintero02@gmail.com>
2022-02-27 11:44:00 -0300

    Aprimoramento dos codigos java

--- Java/src/com/dio/MyFirstProgram.java
+++ Java/src/com/dio/MyFirstProgram.java
@@ -2,10 +2,18 @@ package com.dio;
 
 import com.dio.base.Order;
 
+/**
+* @author guiquintero
+* @version 1.0.0
+* @since Release 1.0.0
+*/
+
+
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-    	Order order = new Order("code1234");
+    	Order order = new Order("code1234", null);
         System.out.println(order);
     }
 }
--- Java/src/com/dio/base/Order.java
+++ Java/src/com/dio/base/Order.java
@@ -1,21 +1,44 @@
 package com.dio.base;
 
+import java.math.BigDecimal;
+
+import javax.management.RuntimeErrorException;
+
 public class Order {
 
 	private final String code;
+	private final BigDecimal totalValue;
 	
-	public Order(String code) {
-		this.code=code;
+	/**
+	 * @param code..........Codigo do pedido
+	 * @param totalValue....Valor total do pedido
+	 * */
+	
+	public Order(String code, BigDecimal totalValue) {
+		this.code= code;
+		this.totalValue= totalValue;
 	}
 
+	/**
+	 * Calcula o valor total de acordo com o valor do pedido, se o valor for maior que 100, uma taxa sera cobrada 
+	 * @return.......... o valor total mais a taxa
+	 * @throws.......... caso o valor do pedido for um valor negativo
+	 * */
+	
+	
+	public BigDecimal calculateFee() throws RuntimeException {
+		if(this.totalValue.signum()<0) {
+			throw new RuntimeErrorException(null, "O pedido nao pode ter valor negativo");
+		}
+		if(this.totalValue.compareTo(new BigDecimal("100.00"))>100) {
+			return this.totalValue.multiply(new BigDecimal("0.99"));
+		}
+		return this.totalValue;
+	}
+	
 	@Override
 	public String toString() {
 		return "Order [code=" + code + "]";
 	}
-	
-	
-	
-	
-	
 	
 }
